89c85f6cecb11e9a7079f5e6c19b1f880933a71f
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2021-03-10 00:06:32 +0700

    Remove debug command. Log information on load

--- OpenDeathMessages/OpenDeathMessages.cs
+++ OpenDeathMessages/OpenDeathMessages.cs
@@ -1,4 +1,6 @@
-﻿using OpenMod.API.Plugins;
+﻿using Cysharp.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using OpenMod.API.Plugins;
 using OpenMod.Unturned.Plugins;
 using System;
 
@@ -9,8 +11,20 @@ namespace EvolutionPlugins.OpenDeathMessages
 {
     public class OpenDeathMessages : OpenModUnturnedPlugin
     {
-        public OpenDeathMessages(IServiceProvider serviceProvider) : base(serviceProvider)
+        private readonly ILogger<OpenDeathMessages> m_Logger;
+
+        public OpenDeathMessages(IServiceProvider serviceProvider, ILogger<OpenDeathMessages> logger) : base(serviceProvider)
         {
+            m_Logger = logger;
+        }
+
+        protected override UniTask OnLoadAsync()
+        {
+            m_Logger.LogInformation($"Made with <3 by {Author}");
+            m_Logger.LogInformation("https://github.com/evolutionplugins \\ https://github.com/diffoz");
+            m_Logger.LogInformation($"Support discord: {Website}");
+
+            return UniTask.CompletedTask;
         }
     }
 }
