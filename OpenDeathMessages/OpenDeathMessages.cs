e8f6372f57b8b67bd0e5d7d903aaa25fe5da292a
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2021-03-09 20:52:48 +0700

    Add project files.

---
+++ OpenDeathMessages/OpenDeathMessages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using Cysharp.Threading.Tasks;
+using OpenMod.Unturned.Plugins;
+using OpenMod.API.Plugins;
+
+[assembly: PluginMetadata("OpenDeathMessages", Author = "EvolutionPlugins", DisplayName = "Open Death Messages",
+    Website = "https://discord.gg/5MT2yke")]
+
+namespace EvolutionPlugins.OpenDeathMessages
+{
+    public class OpenDeathMessages : OpenModUnturnedPlugin
+    {
+        public OpenDeathMessages(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+    }
+}
